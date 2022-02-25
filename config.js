b0f918e55db3592112fd39c4ca24faca8dffc4c4
Hi, my name is Tahubulat <99246083+ahubulat@users.noreply.github.com>
2022-02-26 02:41:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['6281368971807'] // Put your number here
+global.owner = ['6285643774316'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
@@ -32,7 +32,7 @@ global.packname = 'By'
 global.author = ' Vlynn'
 global.email = 'venzvl66@gmail.com'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.wm = '© Vlynn-Md'
+global.wm = '© Tahu-MD'
 global.media = 'https://telegra.ph/file/fb94b67e8562c0be33235.jpg'
 
 global.wait = '_*tunggu sedang di proses...*_'
